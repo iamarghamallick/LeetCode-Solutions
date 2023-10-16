84cf51a446015cb0a53550936cad9111619e47eb
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2023-10-17 00:06:08 +0530

    Time: 4 ms (44.45%), Space: 7.1 MB (23.69%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         for(int i=0; i<s.size(); i++) 
-            t[i+1] += t[i]-s[i];
-        return t[t.length()-1];
+            t[i+1] += t[i]-s[i]; // passing the difference : (t[i]-s[i]) to t[i+1]
+        return t[t.length()-1]; // The difference will be carried over to the last element eventually
     }
 };
