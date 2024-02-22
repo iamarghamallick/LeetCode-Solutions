96a64ebc48d28fa0a060ad56c4ed662b366b0bb4
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-02-22 15:19:22 +0530

    Time: 140 ms (57.65%), Space: 72.2 MB (28.15%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> ans(n, 0);
+
+        for(auto i : trust) {
+            ans[i[0] - 1] = -1;
+
+            if(ans[i[1] - 1] > -1)
+                ++ans[i[1] - 1];
+        }
+
+        for(int i = 0; i < n; i++){
+            if(ans[i] == n - 1)
+                return i + 1;
+        } 
+
+        return -1;
+    }
+};
