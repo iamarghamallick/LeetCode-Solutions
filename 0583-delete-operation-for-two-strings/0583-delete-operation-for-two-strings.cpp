0fe5f7749ef3e3670a950b5bf15d957c997cbece
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-03-11 18:41:22 +0530

    Time: 4 ms (96.55%), Space: 10.2 MB (89.06%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        
+        int dp[m+1][n+1];
+        
+        for(int i=0; i<=m; i++) {
+            for(int j=0; j<=n; j++) {
+                if(i == 0 || j == 0) // initialization
+                    dp[i][j] = 0;
+                    
+                // main logic
+                else if(word1[i-1] == word2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else 
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        return (m - dp[m][n]) + (n - dp[m][n]);
+    }
+};
