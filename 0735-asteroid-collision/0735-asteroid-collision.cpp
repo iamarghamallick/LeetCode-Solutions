0324270995ab55075fc8b4e67571ada2c3e030f4
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-04-01 20:35:24 +0530

    Time: 4 ms (94.85%), Space: 20.6 MB (30.39%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -5,16 +5,33 @@ public:
         stack<int> st;
         st.push(arr[0]);
         
+        // main logic
         for(int i=1; i<n; i++) {
+            // 1st case to handle ->
+            // If the previous asteroid is +ve valued and present one is -ve valued,
+            // i.e. they will collide
+            // Ans also if the present asteroid is larger, previous one will be exploded.
             while(!st.empty() && (st.top() > 0) && (arr[i] < 0) && (st.top() < abs(arr[i])))
                 st.pop();
 
+            // 2nd case -> 
+            // If the previous asteroid is +ve valued and present one is -ve valued,
+            // i.e. they will collide
+            // Now, if the prev and present asteroid is of same size
+            // i.e., both will be exploded, so pop and proceed to the next
             if(!st.empty() && (st.top() > 0) && (arr[i] < 0) && (st.top() == abs(arr[i])))
                 st.pop();
             
-            else if(!st.empty() && ((st.top() <= 0 && arr[i] <= 0) || (st.top()>0 && arr[i]>0) || (st.top()<0 && arr[i]>0)))
+            // 3rd case ->
+            // Check the conditions of NO COLLISION
+                // 1. prev and curr asteroid is both -ve valued, i.e., both moving towards left
+                // 2. prev and curr asteroid is both +ve valued, i.e., both moving towards right
+                // 3. prev asteroid is -ve valued and curr asteroid is +ve valued, i.e., moving to opposite direction
+            // In these cases we just need to push the present asteroid into the stack and proceed to the next
+            else if(!st.empty() && ((st.top() <= 0 && arr[i] <= 0) || (st.top() > 0 && arr[i] > 0) || (st.top() < 0 && arr[i] > 0)))
                 st.push(arr[i]);
             
+            // Otherwise if the stack is empty, just push the present asteroid into the stack
             else if(st.empty())
                 st.push(arr[i]);
                               
