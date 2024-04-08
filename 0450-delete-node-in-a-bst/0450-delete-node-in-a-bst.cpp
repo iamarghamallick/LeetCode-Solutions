0cbef17480553cc516f176468f94cb48b8d2808a
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-04-08 23:15:03 +0530

    Time: 12 ms (98.88%), Space: 32.1 MB (78.54%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -26,18 +26,23 @@ public:
         if(!root)
             return nullptr;
         
-        if(!root->left && !root->right && root->val == key) {
-            // free(root);
+        if(!root->left && !root->right && root->val == key)
             return nullptr;
-        }
         
         if(key < root->val) 
+            // try to delete from left subtree, if found
             root->left = deleteNode(root->left, key);
         
         else if(key > root->val)
+            // try to delete from right subtree, if found
             root->right = deleteNode(root->right, key);
         
         else {
+            // found the key to be deleted
+            
+            // 2 cases arises
+            // case 1 : check if any one of its left or right node is null,
+            //          replace with not null node
             if(!root->left) {
                 TreeNode* temp = root->right;
                 // free(root);
@@ -47,6 +52,11 @@ public:
                 // free(root);
                 return temp;
             }
+            
+            // case 2 : both left and right node exist,
+            //          -> find Inorder Predecessor
+            //          -> replace the value
+            //          -> recursively delete the Inorder Predecessor
             
             TreeNode* iPre = inorderPredecessor(root);
             root->val = iPre->val;
