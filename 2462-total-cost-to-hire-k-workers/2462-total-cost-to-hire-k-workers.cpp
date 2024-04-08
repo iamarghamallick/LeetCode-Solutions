84e2fadf04e78aa0c1906644b0ec7d4338e57f5a
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-04-09 00:01:40 +0530

    Time: 207 ms (13.69%), Space: 76.6 MB (70.97%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        priority_queue<int, vector<int>, greater<int>> leftQ, rightQ;
+        
+        int low = 0, high = costs.size() - 1;
+        for(int i=0; i<candidates; i++)
+            leftQ.push(costs[low++]);
+        for(int i=0; i<candidates && low<=high; i++)
+            rightQ.push(costs[high--]);
+        
+        long long totalCosts = 0;
+        
+        while(k--) {
+            if(!leftQ.empty() && !rightQ.empty() && leftQ.top() <= rightQ.top()) {
+                totalCosts += leftQ.top();
+                cout << leftQ.top() << " ";
+                leftQ.pop();
+                if(low <= high)  leftQ.push(costs[low++]);
+            } else if(!leftQ.empty() && !rightQ.empty()) {
+                totalCosts += rightQ.top();
+                cout << rightQ.top() << " ";
+                rightQ.pop();
+                if(low <= high)  rightQ.push(costs[high--]);
+            } else if(!leftQ.empty()) {
+                totalCosts += leftQ.top();
+                cout << leftQ.top() << " ";
+                leftQ.pop();
+            } else {
+                totalCosts += rightQ.top();
+                cout << rightQ.top() << " ";
+                rightQ.pop();
+            }
+        }
+        
+        return totalCosts;
+    }
+};
