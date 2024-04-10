b6ca189ac54285e95cead6fa29db485e51058677
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-04-10 21:29:23 +0530

    Time: 3 ms (48.10%), Space: 8 MB (58.04%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -3,14 +3,17 @@ public:
     vector<vector<int>> ans;
     
     void solve(int num, int sum, vector<int>& arr, int k, int n) {
+        // Base Case: if we found arr of size k with sum = n
         if(sum == n && k == 0) {
             ans.push_back(arr);
             return;
         }
         
+        // if sum exceeds value n, no greater value is possible to take
         if(sum > n)
             return;
         
+        // otherwise, check for every other combinations [num, 9]
         for(int i = num; i <= 9; i++) {
             if(i > n)
                 return;
