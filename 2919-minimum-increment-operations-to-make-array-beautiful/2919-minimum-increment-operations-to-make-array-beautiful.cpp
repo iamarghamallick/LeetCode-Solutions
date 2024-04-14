e61a15bfa5d95ed1ddc6ea45d5b23216fc24279d
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-04-14 22:00:13 +0530

    Time: 105 ms (48.03%), Space: 84.1 MB (87.34%) - LeetHub

--- 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.cpp
+++ 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    long long dp[100001];
+    
     long long minIncrementOperations(vector<int>& nums, int k) {
-        long long dp1 = 0, dp2 = 0, dp3 = 0, dp;
-        for (int& a: nums) {
-            dp = min(dp1, min(dp2, dp3)) + max(k - a, 0);
-            dp1 = dp2;
-            dp2 = dp3;
-            dp3 = dp;
-        }
-        return min(dp1, min(dp2, dp3));
+        long long n = nums.size();
+        
+        dp[0] = max(0, k - nums[0]);
+        dp[1] = max(0, k - nums[1]);
+        dp[2] = max(0, k - nums[2]);
+        
+        for(long long i=3; i<n; i++)
+            dp[i] = max(0, k - nums[i]) + min(dp[i-1], min(dp[i-2], dp[i-3]));
+        
+        return min(dp[n-1], min(dp[n-2], dp[n-3]));
     }
 };
