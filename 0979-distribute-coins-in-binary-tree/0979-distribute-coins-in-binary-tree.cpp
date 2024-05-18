cab2552714a273ade83c64b6c44e7ff6cbe66918
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-05-18 17:17:40 +0530

    Time: 10 ms (5.12%), Space: 15.6 MB (58.63%) - LeetHub

---
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count;
+    
+    int dfs(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int l = dfs(root->left);
+        int r = dfs(root->right);
+        
+        count += abs(l) + abs(r);
+        
+        return (root->val - 1) + l + r;
+    }
+    
+    int distributeCoins(TreeNode* root) {
+        count = 0;
+        dfs(root);
+        return count;
+    }
+};
