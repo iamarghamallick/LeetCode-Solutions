1ef2ce6246e1cd3ec89c5ec4b35ece04902281ba
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-06-17 18:19:48 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (18.45%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for (int i = 2; i * i <= c; i++) {
+            int count = 0;
+            if (c % i == 0) {
+                while (c % i == 0) {
+                    count++;
+                    c /= i;
+                }
+                if (i % 4 == 3 && count % 2 != 0)
+                    return false;
+            }
+        }
+        return c % 4 != 3;
+    }
+};
