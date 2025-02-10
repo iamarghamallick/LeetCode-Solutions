1d827774b0c1eb92c93ef64c439f79b3e861afbe
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2025-02-10 17:27:44 +0530

    Time: 0 ms, Memory: 8.5 MB - LeetHub

---
+++ 0373-14-3174-clear-digits/0373-14-3174-clear-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans;
+
+        for(int i=0; i<s.size(); i++) {
+            if(isdigit(s[i]) && !ans.empty())
+                ans.pop_back();
+            else
+                ans += s[i];
+        }
+
+        return ans;
+    }
+};
