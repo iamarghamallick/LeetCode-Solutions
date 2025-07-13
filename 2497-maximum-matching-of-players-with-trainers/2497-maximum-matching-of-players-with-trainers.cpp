07dba46551d4472f68a1dd13af151a4a47b5af4b
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2025-07-13 17:28:54 +0530

    Time: 27 ms (77.62%), Space: 80.3 MB (60.41%) - LeetHub

---
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int count = 0;
+        
+        int i=0, j=0;
+        while(i < g.size() && j < s.size()) {
+            if(s[j] >= g[i]) {
+                count++;
+                i++; j++;
+            } else 
+                j++;
+        }
+        
+        return count;
+    }
+
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        return findContentChildren(players, trainers);
+    }
+};
