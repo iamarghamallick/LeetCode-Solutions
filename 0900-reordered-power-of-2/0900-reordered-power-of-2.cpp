f31e264c90e2658feb7b63a8a2ef47851f285739
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2025-08-10 18:29:55 +0530

    Time: 167 ms (5.6%), Space: 8.5 MB (73.16%) - LeetHub

---
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    void solve(vector<int> &nums, vector<int> &curr, int currNum,
+                    vector<bool> &visited, unordered_set<int> &powerOfTwos,
+                    vector<int> &ans) {
+        if(curr.size() == nums.size()) {
+            if(curr[0] != 0 && powerOfTwos.count(currNum)) 
+                ans.push_back(currNum);
+            return;
+        }
+
+        for(int i=0; i<nums.size(); i++) {
+            if(visited[i])
+                continue;
+
+            if(i > 0 && nums[i] == nums[i-1] && !visited[i-1])
+                continue;
+            
+            visited[i] = true;
+            curr.push_back(nums[i]);
+            currNum = currNum * 10 + nums[i];
+
+            solve(nums, curr, currNum, visited, powerOfTwos, ans);
+
+            currNum /= 10;
+            curr.pop_back();
+            visited[i] = false;
+        }
+    }
+
+    bool reorderedPowerOf2(int n) {
+        unordered_set<int> powerOfTwos;
+        long long powerOfTwo = 1;
+        while(powerOfTwo <= (long long)pow(10, 9)) {
+            powerOfTwos.insert(powerOfTwo);
+            powerOfTwo *= 2;
+        }
+
+        vector<int> nums;
+        while(n) {
+            nums.push_back(n % 10);
+            n /= 10;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        vector<int> curr;
+        int currNum = 0;
+        vector<bool> visited(nums.size(), false);
+        vector<int> ans;
+
+        solve(nums, curr, currNum, visited, powerOfTwos, ans);
+
+        return ans.size() >= 1;
+    }
+};
