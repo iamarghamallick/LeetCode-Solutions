1441182a82a5a067c8555ca00562a0ff5f44de70
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2025-09-29 16:18:26 +0530

    Time: 2 ms (99.07%), Space: 41 MB (94.43%) - LeetHub

---
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
@@ -0,0 +1,23 @@
+class Solution {
+    int[] values;
+    int[][] memo;
+
+    public int minScoreTriangulation(int[] values) {
+        this.values = values;
+        int n = values.length;
+        memo = new int[n][n];
+        return dp(0, n-1);
+    }
+
+    public int dp(int i, int j) {
+        if(j < i + 2) return 0;
+        if(memo[i][j] != 0) return memo[i][j];
+        int res = Integer.MAX_VALUE;
+
+        for(int k = i+1; k < j; k++)
+            res = Math.min(res, values[i] * values[k] * values[j] +
+                            dp(i, k) + dp(k, j));
+                            
+        return memo[i][j] = res;
+    }
+}
