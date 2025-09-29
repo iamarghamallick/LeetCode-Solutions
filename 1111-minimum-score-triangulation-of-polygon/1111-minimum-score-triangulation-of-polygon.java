a2932279848342367c165fa7e451ed031e0c6e4d
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2025-09-29 16:18:46 +0530

    Time: 1 ms (100%), Space: 41.1 MB (79.26%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
@@ -6,18 +6,20 @@ class Solution {
         this.values = values;
         int n = values.length;
         memo = new int[n][n];
-        return dp(0, n-1);
+        return dp(0, n - 1);
     }
 
     public int dp(int i, int j) {
-        if(j < i + 2) return 0;
-        if(memo[i][j] != 0) return memo[i][j];
+        if (j < i + 2)
+            return 0;
+        if (memo[i][j] != 0)
+            return memo[i][j];
         int res = Integer.MAX_VALUE;
 
-        for(int k = i+1; k < j; k++)
+        for (int k = i + 1; k < j; k++)
             res = Math.min(res, values[i] * values[k] * values[j] +
-                            dp(i, k) + dp(k, j));
-                            
+                    dp(i, k) + dp(k, j));
+
         return memo[i][j] = res;
     }
 }
