80aa9b0440507632979b09f3a8e1ba9a643223d2
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2026-04-25 16:23:26 +0530

    Time: 0 ms (100%), Space: 61.4 MB (41.53%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n-1-k);
+        reverse(nums, n-k, n-1);
+        reverse(nums, 0, n-1);
+    }
+
+    private void reverse(int[] nums, int i, int j) {
+        while(i <= j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
