06245a9797b8573e90d2795b0f25afb5380275f8
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2026-08-27 18:30:11 +0530

    Time: 3 ms (58.06%), Space: 44.7 MB (77.42%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
@@ -4,20 +4,20 @@ class Solution {
 
     public String lexGreaterPermutation(String s, String target) {
         int[] freq = new int[26];
-        for(char ch: s.toCharArray()) {
+        for (char ch : s.toCharArray()) {
             freq[ch - 'a']++;
         }
 
         result = "";
-        
+
         solve(new StringBuilder(""), freq, target, 0, false);
 
         return result;
     }
 
     private boolean solve(StringBuilder curr, int[] freq, String target, int i, boolean greater) {
-        if(i == target.length()) {
-            if(greater) {
+        if (i == target.length()) {
+            if (greater) {
                 result = curr.toString();
                 return true;
             }
@@ -25,25 +25,25 @@ class Solution {
             return false;
         }
 
-        for(int c=0; c<26; c++) {
-            if(freq[c] == 0) {
+        for (int c = 0; c < 26; c++) {
+            if (freq[c] == 0) {
                 continue;
             }
 
-            if(greater == false && c < target.charAt(i) - 'a') {
+            if (greater == false && c < target.charAt(i) - 'a') {
                 continue;
             }
 
-            curr.append((char)('a' + c));
+            curr.append((char) ('a' + c));
             freq[c]--;
 
             boolean isGreater = greater || c > target.charAt(i) - 'a';
 
-            if(solve(curr, freq, target, i+1, isGreater)) {
+            if (solve(curr, freq, target, i + 1, isGreater)) {
                 return true;
             }
 
-            if(curr.length() > 0) {
+            if (curr.length() > 0) {
                 curr.deleteCharAt(curr.length() - 1);
             }
             freq[c]++;
